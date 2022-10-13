645b944e0c1fb0a1a17c3bae78442b1fb838aaeb
Gonzalo A <gonzaloruiztxurdinaga@gmail.com>
2022-10-13 14:18:35 +0200

    Diseño index PC

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+var busqueda = () => {
+  var input = document.getElementById("searchInput");
+  input.style.visibility = "visible";
+  input.focus();
+}
+
+var buscarLibro = () => {
+  input.style.visibility = "hidden";
+}
