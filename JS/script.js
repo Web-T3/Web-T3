d0d9a356670d72c306b7fbdac56187c1b8c62d1f
Gonzalo A <gonzaloruiztxurdinaga@gmail.com>
2022-10-18 10:43:44 +0200

    Pagina princial v1.00

--- JS/script.js
+++ JS/script.js
@@ -7,3 +7,15 @@ var busqueda = () => {
 var buscarLibro = () => {
   input.style.visibility = "hidden";
 }
+
+
+var imgHover = () => {
+  const TituloLibro = document.querySelector(".tituloLibro");
+  const TituloHover = document.querySelector("#tituloHover");
+  const SinopsisHover = document.querySelector("#sinopsisHover");
+
+  console.log("🚀 ~ file: script.js ~ line 14 ~ imgHover ~ TituloLibro", TituloLibro.value);
+  console.log("🚀 ~ file: script.js ~ line 15 ~ imgHover ~ TituloHover", TituloHover);
+  console.log("🚀 ~ file: script.js ~ line 17 ~ imgHover ~ SinopsisHover", SinopsisHover);
+  
+}
