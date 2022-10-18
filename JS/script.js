18f63d978ed7b6e14601a18a262ab97516e2ad40
Gonzalo A <gonzaloruiztxurdinaga@gmail.com>
2022-10-18 14:18:28 +0200

    JS eventListener

--- JS/script.js
+++ JS/script.js
@@ -1,21 +1,49 @@
-var busqueda = () => {
+window.onload = CargaEventos;
+
+function CargaEventos() {
+  // Accion al darle click a la lupa
+  const LUPA = document.querySelector(".lupa");
+  LUPA.addEventListener("click", Busqueda);
+
+  // Buscar libro
+  const FORMSEARCH = document.querySelector(".search");
+  FORMSEARCH.addEventListener("submit", BuscarLibro);
+
+  // Imagen cuando esta el mouse encima
+  const IMG = document.querySelector(".libro");
+  IMG.addEventListener("mouseover", ImgHover);
+  // Imagen vuelta a lo normal
+  IMG.addEventListener("mouseout", ImgNormal);
+
+}
+
+
+var Busqueda = () => {
   var input = document.getElementById("searchInput");
   input.style.visibility = "visible";
   input.focus();
 }
 
-var buscarLibro = () => {
-  input.style.visibility = "hidden";
+alert("Simyo");
+
+var BuscarLibro = () => {
+  document.querySelector("#searchInput").style.visibility = "hidden";
+}
+
+var ImgHover = () => {
+  const TITULOLIBRO = document.querySelector(".tituloLibro");
+  const TITULOHOVER = document.querySelector("#tituloHover");
+  const SINOPSISHOVER = document.querySelector("#sinopsisHover");
+
+  TITULOHOVER.innerHTML = TITULOLIBRO.innerHTML;
+  SINOPSISHOVER.innerHTML = "";
 }
 
 
-var imgHover = () => {
-  const TituloLibro = document.querySelector(".tituloLibro");
-  const TituloHover = document.querySelector("#tituloHover");
-  const SinopsisHover = document.querySelector("#sinopsisHover");
+var ImgNormal = () => {
+  const TITULOHOVER = document.querySelector("#tituloHover");
+  const SINOPSISHOVER = document.querySelector("#sinopsisHover");
 
-  console.log("🚀 ~ file: script.js ~ line 14 ~ imgHover ~ TituloLibro", TituloLibro.value);
-  console.log("🚀 ~ file: script.js ~ line 15 ~ imgHover ~ TituloHover", TituloHover);
-  console.log("🚀 ~ file: script.js ~ line 17 ~ imgHover ~ SinopsisHover", SinopsisHover);
-  
+  TITULOHOVER.innerHTML = "";
+  SINOPSISHOVER.innerHTML = "";
 }
